1eceb2213796f0b25722d0f22db07583ef8f9bef
virgo4327 <161290510+virgo4327@users.noreply.github.com>
2026-06-02 03:26:31 +0000

    Subida inicial

---
+++ my-angular-app/src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('my-angular-app');
+  
+  hobbies: string[] = ['Fútbol', 'Música', 'Videojuegos', 'Leer'];
+}
